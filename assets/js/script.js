5c9c50ffb9aad90018831f2a1ea4bb5822368bf2
unknown <nurlanmu@code.edu.az>
2024-05-04 13:54:27 +0400

    add css navbar, first section home

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+
+// var dropdown = document.getElementsByClassName("category");
+// var i;
+
+// for (i = 0; i < dropdown.length; i++) {
+//   dropdown[i].addEventListener("mouseover", function() {
+//     this.classList.toggle("active");
+//     var dropdownContent = this.nextElementSibling;
+//     if (dropdownContent.style.display === "block") {
+//       dropdownContent.style.display = "none";
+//     } else {
+//       dropdownContent.style.display = "block";
+//     }
+//   });
+// }
